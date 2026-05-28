d44b1b76eb56f4a67190f2d850aeab9cae54e25e
Souza <b.souza@alvarezandmarsal.com>
2026-05-28 14:56:18 -0300

    Atualizacao automatica dashboard

--- Atualizar Report.bat
+++ Atualizar Report.bat
@@ -1,20 +1,25 @@
 @echo off
+
 cd /d "C:\Users\b.souza\OneDrive - Alvarez and Marsal\Documents\Alvarez & Marsal\GERDAU\Manual_CMG\Report_GitHub"
 
 echo =====================================
 echo Atualizando Dashboard CMG...
 echo =====================================
 
+echo Sincronizando com GitHub...
+git pull origin main
+
+echo Enviando atualizacoes...
 git add .
 
 git commit -m "Atualizacao automatica dashboard"
 
-git push
+git push origin main
 
 echo.
 echo =====================================
-echo Dashboard enviado ao GitHub!
-echo Aguarde 1-3 minutos para atualizar.
+echo Dashboard atualizado!
+echo Aguarde 1-3 minutos.
 echo =====================================
 
 pause
